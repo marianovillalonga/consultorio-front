f4c59298b6011d4b0633fe6c733501c89f131eee
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2026-01-07 13:27:49 -0300

    Arreglo en CORS

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 ﻿const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const getCsrfToken = () => {
   if (typeof document === "undefined") return "";
